ae14163eac4d8bcd647bbd549474c6493908f0d1
Yasas <ysenarath.93@gmail.com>
2021-08-08 19:42:44 -0400

    Update version to v0.0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md") as fh:
 
 setuptools.setup(
     name='Flask-Parcel',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/ysenarath/flask-parcel',
     license='MIT',
     author='Yasas Senarath',
